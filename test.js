436656f2853ef0d8158299c2f3a98e057ab04547
rishabnotfound <itzrishabboss@gmail.com>
2026-03-10 21:55:53 +0530

    headers

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const PROXY_HOST = 'http://localhost:80';
 // Edit these values
 const url = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
 const headers = { 
-  "Origin" : "https://google.com", 
-  "Referer": "https://google.com/" 
+  "Origin" : "https://test-streams.mux.dev", 
+  "Referer": "https://test-streams.mux.dev/" 
 };
 
 const proxyUrl = `${PROXY_HOST}/m3u8-proxy.m3u8?url=${encodeURIComponent(url)}&headers=${encodeURIComponent(JSON.stringify(headers))}`;
